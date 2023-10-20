950119da5f4ded43a24a3e1bba313dd5745e68ad
kevschuetz <kevin.hotz@gmx.at>
2023-10-20 07:38:30 +0200

    init - basic setup (docker, pipeline, swagger)

---
+++ service-registry/src/test/java/at/jku/swe/simcomp/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,15 @@
+package at.jku.swe.simcomp.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
