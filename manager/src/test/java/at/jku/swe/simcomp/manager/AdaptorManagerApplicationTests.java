b2ef13bff2a5ede2b2d548fb5349b61a01a82a71
kevschuetz <kevin.hotz@gmx.at>
2023-11-05 09:44:30 +0100

    adaptor-manager initial commit, general structure

---
+++ manager/src/test/java/at/jku/swe/simcomp/manager/AdaptorManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package at.jku.swe.simcomp.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AdaptorManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
