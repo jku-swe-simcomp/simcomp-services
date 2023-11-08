faedc422e3f90b18eb942d8047af99fb2b91f671
paul2803-byte <feichtenschlager10@icloud.com>
2023-11-08 23:18:39 +0100

    Add new Functions to Webots Robot

---
+++ webots-adaptor/src/main/java/at/jku/swe/simcomp/webotsadaptor/service/command_executors/SetJointExecutor.java
@@ -0,0 +1,32 @@
+package at.jku.swe.simcomp.webotsadaptor.service.command_executors;
+
+import at.jku.swe.simcomp.commons.adaptor.dto.ExecutionResultDTO;
+import at.jku.swe.simcomp.commons.adaptor.dto.JointAngleAdjustmentDTO;
+import at.jku.swe.simcomp.commons.adaptor.dto.JointPositionDTO;
+import at.jku.swe.simcomp.commons.adaptor.execution.command.CommandExecutor;
+import at.jku.swe.simcomp.commons.adaptor.execution.command.ExecutionCommand;
+import at.jku.swe.simcomp.webotsadaptor.domain.simulation.WebotsSimulationConfig;
+import at.jku.swe.simcomp.webotsadaptor.service.WebotsExecutionService;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import org.json.simple.JSONObject;
+
+public class SetJointExecutor
+        implements CommandExecutor<ExecutionCommand.SetJointPositionCommand, WebotsSimulationConfig, ExecutionResultDTO> {
+
+    @SneakyThrows
+    @Override
+    public ExecutionResultDTO execute(ExecutionCommand.SetJointPositionCommand command, WebotsSimulationConfig config) {
+
+        System.out.println("Connecting to " + config.getSimulationEndpointUrl() + " on port " + config.getSimulationPort());
+
+        JSONObject json = new JSONObject();
+        @NonNull JointPositionDTO movement = command.jointPosition(); // now a list is used, maybe change to only send one angle adjustment per command
+        json.put("operation", "set_axis");
+        json.put("axis", movement.getJoint().getIndex());
+        json.put("value", movement.getRadians());
+        return WebotsExecutionService.executeCommand(json, config);
+    }
+}
+
+
