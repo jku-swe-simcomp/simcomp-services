941d31393810d2b4247e3b47094c5d993b4d6a4f
paul2803-byte <feichtenschlager10@icloud.com>
2023-11-08 22:31:59 +0100

    Enable Commands for the Controller

--- webots-adaptor/src/main/java/at/jku/swe/simcomp/webotsadaptor/service/WebotsCommandExecutionVisitor.java
+++ webots-adaptor/src/main/java/at/jku/swe/simcomp/webotsadaptor/service/WebotsCommandExecutionVisitor.java
@@ -4,19 +4,30 @@ import at.jku.swe.simcomp.commons.adaptor.dto.ExecutionResultDTO;
 import at.jku.swe.simcomp.commons.adaptor.endpoint.exception.SessionNotValidException;
 import at.jku.swe.simcomp.commons.adaptor.execution.command.CommandExecutionVisitor;
 import at.jku.swe.simcomp.commons.adaptor.execution.command.ExecutionCommand;
-import at.jku.swe.simcomp.webotsadaptor.service.command_executors.AdjustJointAnglesCommandExecutor;
-import at.jku.swe.simcomp.webotsadaptor.service.command_executors.PoseCommandExecutor;
+import at.jku.swe.simcomp.webotsadaptor.service.command_executors.*;
 
 public class WebotsCommandExecutionVisitor extends CommandExecutionVisitor {
     private final WebotsSessionService demoSessionService;
     private final PoseCommandExecutor poseCommandExecutor;
+    private final GrabExecutor grabExecutor;
+    private final OpenHandExecutor openHandExecutor;
+    private final ResetToHomeExecutor resetToHomeExecutor;
+    private final SetJointExecutor setJointExecutor;
     private final AdjustJointAnglesCommandExecutor adjustJointAnglesCommandExecutor;
     public WebotsCommandExecutionVisitor(WebotsSessionService demoSessionService,
-                                       PoseCommandExecutor poseCommandExecutor,
-                                       AdjustJointAnglesCommandExecutor adjustJointAnglesCommandExecutor){
+                                         PoseCommandExecutor poseCommandExecutor,
+                                         AdjustJointAnglesCommandExecutor adjustJointAnglesCommandExecutor,
+                                         GrabExecutor grabExecutor,
+                                         OpenHandExecutor openHandExecutor,
+                                         ResetToHomeExecutor resetToHomeExecutor,
+                                         SetJointExecutor setJointExecutor){
         this.demoSessionService = demoSessionService;
         this.adjustJointAnglesCommandExecutor = adjustJointAnglesCommandExecutor;
         this.poseCommandExecutor = poseCommandExecutor;
+        this.grabExecutor = grabExecutor;
+        this.openHandExecutor = openHandExecutor;
+        this.resetToHomeExecutor = resetToHomeExecutor;
+        this.setJointExecutor = setJointExecutor;
     }
 
     @Override
