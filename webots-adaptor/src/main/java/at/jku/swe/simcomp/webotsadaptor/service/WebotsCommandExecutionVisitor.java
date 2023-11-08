9503cf91e4c30433563240e69e535741503aa024
paul2803-byte <feichtenschlager10@icloud.com>
2023-11-08 22:55:02 +0100

    Resolve merge conflict

--- webots-adaptor/src/main/java/at/jku/swe/simcomp/webotsadaptor/service/WebotsCommandExecutionVisitor.java
+++ webots-adaptor/src/main/java/at/jku/swe/simcomp/webotsadaptor/service/WebotsCommandExecutionVisitor.java
@@ -2,37 +2,28 @@ package at.jku.swe.simcomp.webotsadaptor.service;
 
 import at.jku.swe.simcomp.commons.adaptor.dto.ExecutionResultDTO;
 import at.jku.swe.simcomp.commons.adaptor.endpoint.exception.SessionNotValidException;
-import at.jku.swe.simcomp.commons.adaptor.execution.command.CommandExecutionVisitor;
+import at.jku.swe.simcomp.commons.adaptor.execution.command.visitor.CommandExecutionVisitor;
 import at.jku.swe.simcomp.commons.adaptor.execution.command.ExecutionCommand;
-import at.jku.swe.simcomp.webotsadaptor.service.command_executors.*;
+import at.jku.swe.simcomp.webotsadaptor.service.command_executors.AdjustJointAngleCommandExecutor;
+import at.jku.swe.simcomp.webotsadaptor.service.command_executors.PoseCommandExecutor;
+import org.springframework.stereotype.Service;
 
+@Service
 public class WebotsCommandExecutionVisitor extends CommandExecutionVisitor {
     private final WebotsSessionService demoSessionService;
     private final PoseCommandExecutor poseCommandExecutor;
-    private final GrabExecutor grabExecutor;
-    private final OpenHandExecutor openHandExecutor;
-    private final ResetToHomeExecutor resetToHomeExecutor;
-    private final SetJointExecutor setJointExecutor;
-    private final AdjustJointAnglesCommandExecutor adjustJointAnglesCommandExecutor;
+    private final AdjustJointAngleCommandExecutor adjustJointAngleCommandExecutor;
     public WebotsCommandExecutionVisitor(WebotsSessionService demoSessionService,
                                          PoseCommandExecutor poseCommandExecutor,
-                                         AdjustJointAnglesCommandExecutor adjustJointAnglesCommandExecutor,
-                                         GrabExecutor grabExecutor,
-                                         OpenHandExecutor openHandExecutor,
-                                         ResetToHomeExecutor resetToHomeExecutor,
-                                         SetJointExecutor setJointExecutor){
+                                         AdjustJointAngleCommandExecutor adjustJointAngleCommandExecutor){
         this.demoSessionService = demoSessionService;
-        this.adjustJointAnglesCommandExecutor = adjustJointAnglesCommandExecutor;
+        this.adjustJointAngleCommandExecutor = adjustJointAngleCommandExecutor;
         this.poseCommandExecutor = poseCommandExecutor;
-        this.grabExecutor = grabExecutor;
-        this.openHandExecutor = openHandExecutor;
-        this.resetToHomeExecutor = resetToHomeExecutor;
-        this.setJointExecutor = setJointExecutor;
     }
 
     @Override
-    public ExecutionResultDTO visit(ExecutionCommand.AdjustJointAnglesCommand command, String sessionKey) throws SessionNotValidException {
-        return adjustJointAnglesCommandExecutor.execute(command, demoSessionService.renewSession(sessionKey));
+    public ExecutionResultDTO visit(ExecutionCommand.AdjustJointAngleCommand command, String sessionKey) throws SessionNotValidException {
+        return adjustJointAngleCommandExecutor.execute(command, demoSessionService.renewSession(sessionKey));
     }
 
     @Override
--- webots-adaptor/src/main/java/at/jku/swe/simcomp/webotsadaptor/service/command_executors/PoseCommandExecutor.java
+++ webots-adaptor/src/main/java/at/jku/swe/simcomp/webotsadaptor/service/command_executors/PoseCommandExecutor.java
@@ -1,22 +1,29 @@
 package at.jku.swe.simcomp.webotsadaptor.service.command_executors;
 
 import at.jku.swe.simcomp.commons.adaptor.dto.ExecutionResultDTO;
+import at.jku.swe.simcomp.commons.adaptor.dto.PoseDTO;
+import at.jku.swe.simcomp.commons.adaptor.dto.RoboStateDTO;
 import at.jku.swe.simcomp.commons.adaptor.execution.command.CommandExecutor;
 import at.jku.swe.simcomp.commons.adaptor.execution.command.ExecutionCommand;
 import at.jku.swe.simcomp.webotsadaptor.domain.simulation.WebotsSimulationConfig;
-import at.jku.swe.simcomp.webotsadaptor.service.WebotsExecutionService;
-import lombok.SneakyThrows;
-import org.json.simple.JSONObject;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 
+@Service
 public class PoseCommandExecutor implements CommandExecutor<ExecutionCommand.PoseCommand, WebotsSimulationConfig, ExecutionResultDTO> {
 
-    @SneakyThrows
     @Override
     public ExecutionResultDTO execute(ExecutionCommand.PoseCommand command, WebotsSimulationConfig config) {
-
-        JSONObject json = new JSONObject();
-        json.put("operation", "get_position");
-        return WebotsExecutionService.executeCommand(json, config);
+        return ExecutionResultDTO.builder()
+                .report("Pose command executed")
+                .currentState(RoboStateDTO.builder()
+                        .pose(PoseDTO.builder()
+                                .position(command.position())
+                                .orientation(command.orientation())
+                                .build())
+                        .jointStates(new ArrayList<>())
+                        .build())
+                .build();
     }
 }
