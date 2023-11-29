da5b3a996b7cd147dd451eb6349a684f1ce5d368
paul2803-byte <feichtenschlager10@icloud.com>
2023-11-29 16:54:00 +0100

    Implementation of Converter Service

---
+++ axis-converter/src/main/java/at/jku/swe/simcomp/axisconverter/AxisConverterApplication.java
@@ -0,0 +1,11 @@
+package at.jku.swe.simcomp.axisconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxisConverterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AxisConverterApplication.class, args);
+    }
+}
