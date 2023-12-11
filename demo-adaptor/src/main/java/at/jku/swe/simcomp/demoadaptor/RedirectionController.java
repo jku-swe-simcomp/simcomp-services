9cb2f2bc1c194c4b0aaa102f519c4a6ca0fc34e0
kevschuetz <kevin.hotz@gmx.at>
2023-12-11 10:35:30 +0100

    controllers that redirect to swagger by default for all spring boot projects added

---
+++ demo-adaptor/src/main/java/at/jku/swe/simcomp/demoadaptor/RedirectionController.java
@@ -0,0 +1,15 @@
+package at.jku.swe.simcomp.demoadaptor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RedirectionController {
+
+    @GetMapping
+    public String redirectToSwagger(){
+        return "redirect:/swagger-ui/index.html";
+    }
+}
