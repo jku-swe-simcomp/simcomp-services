4248ea0a2e9fb0fbfb0692a779c17b8954fda222
paul2803-byte <feichtenschlager10@icloud.com>
2023-12-29 14:44:54 +0100

    Implementation of Inverse-Kinematics

--- axis-converter/src/main/java/at/jku/swe/simcomp/axisconverter/AxisConverterController.java
+++ axis-converter/src/main/java/at/jku/swe/simcomp/axisconverter/AxisConverterController.java
@@ -6,20 +6,25 @@ import at.jku.swe.simcomp.axisconverter.service.InverseKinematics;
 import at.jku.swe.simcomp.commons.adaptor.dto.JointPositionDTO;
 import at.jku.swe.simcomp.commons.adaptor.dto.PoseDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/converter")
 @Slf4j
 public class AxisConverterController {
 
-    // TODO: change to other DTO
     @PostMapping("/poseToAxis")
     public List<JointPositionDTO> convertAxesToPosition(@RequestBody PoseDTO position) {
+        System.out.println(position);
         return InverseKinematics.inverseKinematics(position);
+    }
+
+    @PostMapping("/poseToAxis/{granularity}")
+    public List<JointPositionDTO> convertAxesToPosition(@RequestBody PoseDTO position, @PathVariable int granularity) {
+        return InverseKinematics.inverseKinematics(position, granularity);
     }
 
     @PostMapping("/axisToPose")
